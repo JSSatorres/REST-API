01d966d8499fafef1bd1f177f5101bbe5d2e7aa3
JSSatorres <juansataz.dev@gmail.com>
2025-03-27 12:41:28 +0100

    feat: refactor UserCreator to use UserCreatorRequest and value objects for user creation

--- src/Context/User/application/UserCreator.ts
+++ src/Context/User/application/UserCreator.ts
@@ -1,11 +1,22 @@
 import { User } from '../domain/User'
 import { UserRepository } from '../domain/UserRepository'
+import { UserCreatorRequest } from './dto/UserCreatorRequest'
+import { UserId } from '../domain/UserId'
+import { UserName } from '../domain/UserName'
+import { UserEmail } from '../domain/UserEmail'
+import { UserPassword } from '../domain/UserPassword'
 
 export class UserCreator {
-  constructor(private userRepository: UserRepository) {}
+  constructor(private readonly userRepository: UserRepository) {}
 
-  async run(id: string, name: string, email: string, password: string): Promise<void> {
-    const user = new User(id, name, email, password)
+  async run(request: UserCreatorRequest): Promise<void> {
+    const { id, name, email, password } = request
+    const user = new User({
+      id: new UserId(id),
+      name: new UserName(name),
+      email: new UserEmail(email),
+      password: new UserPassword(password)
+    })
 
     return this.userRepository.save(user)
   }
