31e2f0959b964e5f30410cf20e7ec3e360694168
JSSatorres <juansataz.dev@gmail.com>
2025-04-11 09:42:24 +0200

    chore: remove user routes and Swagger setup files

--- src/app/routes/user.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express'
-import container from '../dependency-injection/container'
-import { UserPutController } from '../controllers/UserPutController'
-
-export const register = (router: Router) => {
-  const controlelr: UserPutController = container.resolve('userPutController')
-  router.put('/user/:id', (req, res) => controlelr.run(req, res))
-}
